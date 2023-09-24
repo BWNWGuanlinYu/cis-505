ccbca5ddaddd522f51966d38a20390f5f4f6b2c1
BWNWGuanlinYu <143769360+BWNWGuanlinYu@users.noreply.github.com>
2023-09-24 20:33:11 +0800

    Update GenericQueue.java

--- Module_4/GenericQueue.java
+++ Module_4/GenericQueue.java
@@ -3,10 +3,12 @@ import java.util.LinkedList;
 public class GenericQueue<E> {
     private LinkedList<E> list = new LinkedList<E>();
 
+    // Enqueue an item to the queue
     public void enqueue(E item) {
         list.addFirst(item);
     }
 
+    // Dequeue and return the first item from the queue
     public E dequeue() {
         if (!isEmpty()) {
             return list.removeFirst();
@@ -15,10 +17,12 @@ public class GenericQueue<E> {
         }
     }
 
+    // Get the size of the queue
     public int size() {
         return list.size();
     }
 
+    // Check if the queue is empty
     public boolean isEmpty() {
         return list.isEmpty();
     }
