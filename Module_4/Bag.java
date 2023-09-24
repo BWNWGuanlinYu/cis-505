28f6734e305111604c7b1e18550adaa3e22366b6
BWNWGuanlinYu <143769360+BWNWGuanlinYu@users.noreply.github.com>
2023-09-24 20:33:22 +0800

    Update Bag.java

--- Module_4/Bag.java
+++ Module_4/Bag.java
@@ -1,8 +1,11 @@
 public class Bag extends Product {
     private String type = "";
 
+    // Constructor
     public Bag() {
     }
+
+    // Getter and Setter method for type
 
     public String getType() {
         return type;
@@ -12,6 +15,7 @@ public class Bag extends Product {
         this.type = type;
     }
 
+    // Override toString method to include type information
     @Override
     public String toString() {
         return super.toString() + "\nType: " + type;
